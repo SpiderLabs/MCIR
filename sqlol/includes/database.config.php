0dd93ac0080e19eb0feffb4ba6b56cc49a2dc323
Daniel "unicornFurnace" Crowley <dan.crowley+github@gmail.com>
2016-05-20 13:45:47 -0400

    MySQL -> MySQLi
    MySQL connector is deprecated in PHP 5.5.0 and removed in PHP 7.0.0, switching over to mysqli

--- sqlol/includes/database.config.php
+++ sqlol/includes/database.config.php
@@ -16,7 +16,7 @@ You should have received a copy of the GNU General Public License along with thi
 //Choose the appropriate type of database here
 
 //Choose me for MySQL
-$dbtype = 'mysql';
+$dbtype = 'mysqli';
 $server = 'localhost';
 $port = '3306';
 $username = 'root';
