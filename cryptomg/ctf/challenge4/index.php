f401bbe63c9156f46418d2db90179a645d7a44ee
Daniel "unicornFurnace" Crowley <dan.crowley+github@gmail.com>
2016-05-20 13:56:52 -0400

    Update index.php

--- cryptomg/ctf/challenge4/index.php
+++ cryptomg/ctf/challenge4/index.php
@@ -36,9 +36,9 @@ function checkAuth($username, $password){
 		return true;
 	}else{
 		$sql_check_auth = "SELECT * FROM challenge4_users WHERE username='$username'";
-		$query_check_auth = mysql_query($sql_check_auth) or die(mysql_error());
-		if(mysql_num_rows($query_check_auth)){
-			$result = mysql_fetch_array($query_check_auth);
+		$query_check_auth = mysqli_query($sql_check_auth) or die(mysqli_error());
+		if(mysqli_num_rows($query_check_auth)){
+			$result = mysqli_fetch_array($query_check_auth);
 			if($result['password'] == $password){
 				$authtoken = $result['id']."|".$result['email']."|".$result['username'];
 				$GLOBALS['username2'] = $result['username'];
